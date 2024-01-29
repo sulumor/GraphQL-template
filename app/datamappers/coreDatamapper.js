4b7a780ad924cd9807d63aefe445d8e77f78a135
sulumor <romuald.patfoort@gmail.com>
2024-01-29 12:33:11 +0100

    docs: add docs in coreDatamapper

--- app/datamappers/coreDatamapper.js
+++ app/datamappers/coreDatamapper.js
@@ -3,8 +3,19 @@ class CoreDatamapper {
 
   constructor(client) {
     this.client = client;
+    this.findByPkBatch = this.client.query
+      .select("*")
+      .batch(async (query, batchIds) => {
+        const rows = await query.from(this.tableName).whereIn("id", batchIds).cache(process.env.CACHE_TLL);
+        return batchIds.map((batchId) => rows?.find((row) => row.id === batchId));
+      });
   }
 
+  /**
+   *  Find all registrations or a list of regsitrations with a specific condition
+   * @param {{where : Object}} [params] Optionnal parameter to have a condition in the query
+   * @returns An array of registrations
+   */
   async findAll(params) {
     const query = this.client.query.from(this.tableName);
     if (params?.where) query.where(params.where);
@@ -12,6 +23,11 @@ class CoreDatamapper {
     return rows;
   }
 
+  /**
+   *  Find one registration with a specific id
+   * @param {number} id Identifier
+   * @returns One registration
+   */
   async findByPk(id) {
     const rows = await this.client.query
       .from(this.tableName)
@@ -21,6 +37,11 @@ class CoreDatamapper {
     return rows;
   }
 
+  /**
+   * Create a new registration in the data base
+   * @param {Object} inputData All informations to insert in the database a new registration
+   * @returns The registration newly insert
+   */
   async create(inputData) {
     const row = await this.client.query
       .into(this.tableName)
@@ -29,6 +50,12 @@ class CoreDatamapper {
     return row;
   }
 
+  /**
+   * Update one registration by id
+   * @param {{ id: number}} id Identifier of the registration to update
+   * @param {Object} inputData A list of params to be update
+   * @returns The registration newly updated
+   */
   async update({ id }, inputData) {
     const row = await this.client.query
       .from(this.tableName)
@@ -38,6 +65,11 @@ class CoreDatamapper {
     return row;
   }
 
+  /**
+ * Delete one registration by id
+ * @param {number} id Identifier of the registration to update
+ * @returns boolean if registration is delete or not
+ */
   async delete(id) {
     const rowCount = await this.client.query
       .from(this.tableName)
